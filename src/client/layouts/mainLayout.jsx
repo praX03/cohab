c51fa6da577fab679ed797427807b79593a3de4c
ItheNerd <99102582+ItheNerd@users.noreply.github.com>
2023-04-02 00:31:07 +0530

    Added waitList and background illustration to hero section

--- src/client/layouts/mainLayout.jsx
+++ src/client/layouts/mainLayout.jsx
@@ -1,13 +1,18 @@
-import React from "react";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
-function Layout({ children, otherChild }) {
+function Layout({ children, otherChild, showNav, showFooter, classStyles }) {
   return (
     <>
-      <Navbar />
-      <main className="">{children}</main>
-      <Footer />
+      {showNav ? <Navbar /> : <></>}
+      <main className={classStyles}>{children}</main>
+      {showFooter ? <Footer /> : <></>}
     </>
   );
 }
+
+Layout.defaultProps = {
+  showNav: true,
+  showFooter: true,
+};
+
 export default Layout;
